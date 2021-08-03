b29b10763eaafef6e00ca4bb54d81ab26e9b56df
Svenden123 <buhrus1@yandex.ru>
2021-08-03 19:48:30 +0400

    first commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#nested_list = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10, 11, 12, 13]]
+#print(nested_list)
+#print(len(nested_list))
+#print(len(nested_list[1]))
+#print(len(nested_list[-1]))
+#print((nested_list)[1][2])
+
+#print((nested_list)[3][2])
+#print((nested_list)[-1][2])
+#print((nested_list)[3][-2])
+#print((nested_list)[-1][-2])
+
+#for inner_list in nested_list:
+    #print(inner_list)
+
+#for inner_list in nested_list:
+    #for number in inner_list:
+        #print(number)
+
+
+nested_list = [[1, 2, 3], [4, 5, 6, True], [7, 8, 9, 'text'], [10, 11, 12, 13], [False, 23, 'hello']]
+[[print(number) for number in inner_list] for inner_list in nested_list]
